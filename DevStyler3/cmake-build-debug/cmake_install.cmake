7b2b415b8ddd45e1b85e75fa0b92f60eeab27a09
Dobrolyub Velinov <dvelinov_kn@abv.bg>
2024-01-25 14:14:30 +0200

    change folder

--- DevStyler3/cmake-build-debug/cmake_install.cmake
+++ DevStyler3/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Dobrolyub Velinov/CLionProjects/Demos (3)
+# Install script for directory: C:/Users/Dobrolyub Velinov/Cpp-projects/DevStyler3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Dobrolyub Velinov/CLionProjects/Demos (3)/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Dobrolyub Velinov/Cpp-projects/DevStyler3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
