f29f3a148094e0e32a0a98b09ace00da451be240
Dobrolyub Velinov <dvelinov_kn@abv.bg>
2024-01-29 12:36:01 +0200

    Change folders

---
+++ Demo/DevStyler/DerivedMemberFunction/BasePlusCommissionEmployee.h
@@ -0,0 +1,27 @@
+// BasePlusCommissionEmployee class derived from class 
+// CommissionEmployee.
+#ifndef BASEPLUS_H
+#define BASEPLUS_H
+
+#include <string> // C++ standard string class
+using std::string;
+
+#include "CommissionEmployee.h" // CommissionEmployee class declaration
+
+class BasePlusCommissionEmployee : public CommissionEmployee
+{
+public:
+   BasePlusCommissionEmployee( const string &, const string &, 
+      const string &, double = 0.0, double = 0.0, double = 0.0 );
+   
+   void setBaseSalary( double ); // set base salary
+   double getBaseSalary() const; // return base salary
+
+   double earnings() const; // calculate earnings
+   void print() const; // print BasePlusCommissionEmployee object
+private:
+   double baseSalary; // base salary
+}; // end class BasePlusCommissionEmployee
+
+#endif
+
