a686745bddd216f2ab1523593dde460ce2512354
Dobrolyub Velinov <dvelinov_kn@abv.bg>
2024-02-05 20:02:47 +0200

    Change folders

--- OOP/Regtangle/cmake-build-debug/cmake_install.cmake
+++ OOP/Regtangle/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Dobrolyub Velinov/CPP-Projects/OPP/Regtangle
+# Install script for directory: C:/Users/Dobrolyub Velinov/CPP-Projects/OOP/Regtangle
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Dobrolyub Velinov/CPP-Projects/OPP/Regtangle/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Dobrolyub Velinov/CPP-Projects/OOP/Regtangle/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
